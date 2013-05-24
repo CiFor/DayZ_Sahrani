1ea42979ebdec0d3a6ca725e277bfae42ad0047a
Michael Larkin <seven@ehdgaming.co.uk>
2013-05-24 02:46:45 +0100

    New Version.

--- New_Buildings/PHP CFGTownGenerator Converter/cfgtown.php
+++ New_Buildings/PHP CFGTownGenerator Converter/cfgtown.php
@@ -1,47 +1,83 @@
 <?php
-/*
-	mission.biedi to CFGTownGenerator
-*/
-//Function to Generate Code
+/*******************************/
+/* CFGTownGenerator PHP Parser */
+/*      Created by Seven       */
+/*******************************/
+function getClasses($file){
+	$classes = array("POSITION"=>null, "TYPE"=>null, "AZIMUT"=>null);
+	$class = array();
+	$objects = array();
+	$total = 0;
+	$skippedJunk = false;
+	$file = $file.".Sara/mission.biedi";
+	//Check if file exists.
+	if(File_Exists($file)) {
+		//Split into lines
+		$contents = explode("\n",file_get_contents($file));
+	}
+	foreach ($contents as $line) {
+		//Cleanup any spaces in each line
+		$line = trim($line);
+		$parts = array();
+		//Remove blank lines
+		if(empty($line) || $line == ""){
+			continue;
+		}
+		//Find start of object
+		if($line == 'objectType="vehicle";') {
+			$class[$total] = array("POSITION"=>"0,0,0", "TYPE"=>"blank", "AZIMUT"=>"0");
+			$skippedJunk = true;
+			continue;
+		}
+		//Don't Parse until we find the start string
+		if(!$skippedJunk) 
+			continue;
+		//Find end of Object and reset everything
+		if($line == "};") {
+			$objects[count($objects)] = $class[$total];
+			$total++;
+			$skippedJunk = false;
+			continue;
+		}
+		//Split the var from the value
+		if(strpos($line,"="))
+			$parts = explode("=", $line);
+		//Cleanup and add them to the class array
+		if( count($parts) > 0 && array_key_exists(trim($parts[0]), $classes)) {
+			if($parts[0] == "POSITION")
+				$parts[1] = substr($parts[1],2,-3);
+			if($parts[0] == "TYPE")
+				$parts[1] = substr($parts[1],1,-2);
+			if($parts[0] == "AZIMUT")
+				(float)$parts[1] = substr($parts[1],1,-2);
+			$class[$total][ trim($parts[0]) ] = trim( $parts[1] );
+		}
+	}
+	return $objects;
+}
+
 function GenerateCFGTown($zone) {
-	//Extremely Ugly and poor Regx!
-	$reg = '/class\x20_vehicle_[a-zA-Z0-9\.-_]*\r\n\{\r\n\t.*\r\n.*\r\n\t{\r\n\t\tPOSITION="\[(?P<loc1>[0-9\.\-]*),\x20(?P<loc2>[0-9\.\-]*),\x20(?P<loc3>[0-9\.\-]*).*\r\n\t\tTYPE="(?P<type>.*)";\r\n\t\tAZIMUT="(?P<yaw>[0-9\.\-e]*)";/';
 	//Loop through all the zone
 	for($i=0;$i<count($zone);$i++) {
-		//Check if there is a file for the zone
-		if(File_Exists($zone[$i][0].".biedi")) {
-			//Get contents of zone file
-			$sourcestring = file_get_contents($zone[$i][0].".biedi");
-			preg_match_all($reg, $sourcestring, $data);
-			//Collect the data
-			$x = $data[1];
-			$y = $data[2];
-			(float)$z =$data[3];
-			$types = $data[4];
-			$yaw = $data[5];
+	//Check if there is a file for the zone
+		if(File_Exists($zone[$i][0].".Sara/mission.biedi")) {
 			//Start Generating class for zone
 			echo "	class ".$zone[$i][0]." {<br>";
 			echo "		position[] = ".$zone[$i][1].";<br>";
 			echo "		size = ".$zone[$i][2].";<br><br>";
 			//Generate class for building
-			for($j=0;$j<count($x);$j++) {
-				$tmpx = $x[$j];
-				$tmpy = $y[$j];
-				$tmpz = $z[$j];
-				if($tmpz > 1 || $tmpz < -1) $tmpz = 0;
-				$tmptype = $types[$j];
-				$tmpyaw = $yaw[$j];
+			$classes = getClasses($zone[$i][0]);
+			for($j=0;$j<count($classes);$j++) {
 				echo "		class Object".$j." {<br>";
-				echo "			Type=\"".$tmptype."\";<br>";
-				echo "			position[] = {".$tmpx.",".$tmpy.",".$tmpz."};<br>";
-				echo "			direction = ".$tmpyaw.";<br>";
+				echo "			Type=\"".$classes[$j]["TYPE"]."\";<br>";
+				echo "			position[] = {".$classes[$j]["POSITION"]."};<br>";
+				echo "			direction = ".$classes[$j]["AZIMUT"].";<br>";
 				echo "			SetZUp = 1;<br>		};<br>";
 			}
 			echo "	};<br>";
 		}
 	}
 }
-/*Sahrani Zones*/
 $zones[0] 				= array("NWI","{3549.5293, 16948.412}","2750");;
 $zones[count($zones)]	= array("EMLZ1","{9084.665, 15606.263}","4250");
 $zones[count($zones)]	= array("EMLZ2","{13508.527, 16287.334}","3000");
@@ -59,7 +95,6 @@ $zones[count($zones)]	= array("WMLZ6","{9737.9395, 7844.0952}","1200");
 $zones[count($zones)]	= array("NEI","{17719.813, 18512.668}","1800");
 $zones[count($zones)]	= array("SWI","{2596.4631, 2593.689}","1900");
 $zones[count($zones)]	= array("SEI","{17488.941, 3990.011}","1800");
-
 /*Run The Generator*/
 echo "<pre>";
 	GenerateCFGTown($zones);
