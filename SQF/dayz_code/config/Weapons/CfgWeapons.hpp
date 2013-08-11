8802528bb0e231f7111713eec25f9eb7ba0b2a18
Michael Larkin <seven@ehdgaming.co.uk>
2013-08-11 20:40:03 +0100

    Weapon Fixes

--- SQF/dayz_code/config/Weapons/CfgWeapons.hpp
+++ SQF/dayz_code/config/Weapons/CfgWeapons.hpp
@@ -452,6 +452,289 @@ class CfgWeapons {
 		};
 		descriptionShort = $STR_DSS_MAKAROVSD;
 	};
+	
+		class PK_DZ : Rifle {
+		bullet1[] = {"ca\sounds\weapons\shells\big_shell_metal_01", 0.177828, 1, 15};
+		bullet2[] = {"ca\sounds\weapons\shells\big_shell_metal_03", 0.177828, 1, 15};
+		bullet3[] = {"ca\sounds\weapons\shells\big_shell_metal_02", 0.177828, 1, 15};
+		bullet4[] = {"ca\sounds\weapons\shells\big_shell_metal_01", 0.177828, 1, 15};
+		bullet5[] = {"ca\sounds\weapons\shells\big_shell_dirt_01", 0.316228, 1, 15};
+		bullet6[] = {"ca\sounds\weapons\shells\big_shell_dirt_02", 0.316228, 1, 15};
+		bullet7[] = {"ca\sounds\weapons\shells\big_shell_dirt_03", 0.316228, 1, 15};
+		bullet8[] = {"ca\sounds\weapons\shells\big_shell_dirt_04", 0.316228, 1, 15};
+		bullet9[] = {"ca\sounds\weapons\shells\big_shell_soft_01", 0.177828, 1, 15};
+		bullet10[] = {"ca\sounds\weapons\shells\big_shell_soft_02", 0.177828, 1, 15};
+		bullet11[] = {"ca\sounds\weapons\shells\big_shell_soft_03", 0.177828, 1, 15};
+		bullet12[] = {"ca\sounds\weapons\shells\big_shell_soft_04", 0.177828, 1, 15};
+		soundBullet[] = {"bullet1", 0.083, "bullet2", 0.083, "bullet3", 0.083, "bullet4", 0.083, "bullet5", 0.083, "bullet6", 0.083, "bullet7", 0.083, "bullet8", 0.083, "bullet9", 0.083, "bullet10", 0.083, "bullet11", 0.083, "bullet12", 0.083};
+		scope = public;
+		model = "\ca\weapons\PK_MG_proxy";
+		displayName = $STR_DN_PK;
+		dexterity = 0.96;
+		picture = "\CA\weapons\data\equip\w_pk_ca.paa";
+		UiPicture = "\CA\weapons\data\Ico\i_mg_CA.paa";
+		cursor = "MGCursor";
+		cursoraim = "\ca\Weapons\Data\clear_empty";
+		reloadMagazineSound[] = {"\Ca\sounds\Weapons\rifles\reload-belt-1", 0.0562341, 1, 20};
+		modes[] = {"manual", "close", "short", "medium", "far"};
+		
+		class manual : Mode_FullAuto {
+			reloadTime = 0.09;
+			recoil = "recoil_auto_machinegun_8outof10";
+			recoilProne = "recoil_auto_machinegun_prone_5outof10";
+			dispersion = 0.001;
+			begin1[] = {"ca\sounds\weapons\machineguns\PKM_single2", 1.41254, 1, 1200};
+			begin2[] = {"ca\sounds\weapons\machineguns\PKM_single3", 1.41254, 1, 1200};
+			soundBegin[] = {"begin1", 0.3, "begin2", 0.7};
+			soundContinuous = 0;
+			soundBurst = 0;
+			minRange = 0;
+			minRangeProbab = 0.3;
+			midRange = 5;
+			midRangeProbab = 0.58;
+			maxRange = 10;
+			maxRangeProbab = 0.04;
+			showToPlayer = true;
+		};
+		
+		class close : manual {
+			burst = 10;
+			aiRateOfFire = 0.5;	// delay between shots at given distance
+			aiRateOfFireDistance = 50;
+			minRange = 10;
+			minRangeProbab = 0.05;
+			midRange = 20;
+			midRangeProbab = 0.58;
+			maxRange = 50;
+			maxRangeProbab = 0.04;
+			showToPlayer = false;
+		};
+		
+		class short : close {
+			burst = 8;
+			aiRateOfFire = 2;	// delay between shots at given distance
+			aiRateOfFireDistance = 300;
+			minRange = 50;
+			minRangeProbab = 0.05;
+			midRange = 150;
+			midRangeProbab = 0.58;
+			maxRange = 300;
+			maxRangeProbab = 0.04;
+		};
+		
+		class medium : close {
+			burst = 12;
+			aiRateOfFire = 3;	// delay between shots at given distance
+			aiRateOfFireDistance = 600;
+			minRange = 200;
+			minRangeProbab = 0.05;
+			midRange = 400;
+			midRangeProbab = 0.58;
+			maxRange = 600;
+			maxRangeProbab = 0.04;
+		};
+		
+		class far : close {
+			burst = 16;
+			aiRateOfFire = 5;	// delay between shots at given distance
+			aiRateOfFireDistance = 1000;
+			minRange = 400;
+			minRangeProbab = 0.05;
+			midRange = 600;
+			midRangeProbab = 0.4;
+			maxRange = 900;
+			maxRangeProbab = 0.01;
+		};
+		aiDispersionCoefY = 21.0;
+		aiDispersionCoefX = 21.0;
+		type = 1	 + 	4;
+		magazines[] = {"100Rnd_762x54_PK"};
+		
+		class Library {
+			libTextDesc = $STR_LIB_PK;
+		};
+		descriptionShort = $STR_DSS_PK;
+	};
+	
+	class Pecheneg_DZ : PK {
+		model = "\ca\weapons\Pecheneg_MG_proxy";
+		displayName = $STR_DN_PECHENEG;
+		dexterity = 0.7;
+		picture = "\CA\weapons\data\equip\w_pecheneg_mg_ca.paa";
+		modelOptics = "\ca\weapons\2Dscope_Pecheneg";
+		opticsPPEffects[] = {"OpticsCHAbera2", "OpticsBlur3"};
+		opticsFlare = true;
+		opticsDisablePeripherialVision = true;
+		opticsZoomInit = 0.0623;
+		opticsZoomMin = 0.0623;
+		opticsZoomMax = 0.0623;
+		distanceZoomMin = 300;
+		distanceZoomMax = 300;
+		modes[] = {"manual", "close", "short", "medium", "far"};
+		
+		class manual : manual {
+			minRange = 0;
+			minRangeProbab = 0.3;
+			midRange = 5;
+			midRangeProbab = 0.58;
+			maxRange = 10;
+			maxRangeProbab = 0.04;
+		};
+		
+		class close : close {
+			minRange = 10;
+			minRangeProbab = 0.05;
+			midRange = 20;
+			midRangeProbab = 0.58;
+			maxRange = 50;
+			maxRangeProbab = 0.04;
+		};
+		
+		class short : short {
+			minRange = 50;
+			minRangeProbab = 0.05;
+			midRange = 200;
+			midRangeProbab = 0.58;
+			maxRange = 400;
+			maxRangeProbab = 0.04;
+		};
+		
+		class medium : medium {
+			minRange = 200;
+			minRangeProbab = 0.05;
+			midRange = 400;
+			midRangeProbab = 0.58;
+			maxRange = 600;
+			maxRangeProbab = 0.04;
+		};
+		
+		class far : far {
+			minRange = 400;
+			minRangeProbab = 0.05;
+			midRange = 600;
+			midRangeProbab = 0.4;
+			maxRange = 1000;
+			maxRangeProbab = 0.01;
+		};
+		
+		class Library {
+			libTextDesc = $STR_LIB_PECHENEG;
+		};
+		descriptionShort = $STR_DSS_PECHENEG;
+	};
+	
+	class M60A4_EP1 : Rifle {
+		bullet1[] = {"ca\sounds\weapons\shells\big_shell_metal_01", 0.0707946, 1, 15};
+		bullet2[] = {"ca\sounds\weapons\shells\big_shell_metal_03", 0.0707946, 1, 15};
+		bullet3[] = {"ca\sounds\weapons\shells\big_shell_metal_02", 0.0707946, 1, 15};
+		bullet4[] = {"ca\sounds\weapons\shells\big_shell_metal_01", 0.0707946, 1, 15};
+		bullet5[] = {"ca\sounds\weapons\shells\big_shell_dirt_01", 0.0707946, 1, 15};
+		bullet6[] = {"ca\sounds\weapons\shells\big_shell_dirt_02", 0.0707946, 1, 15};
+		bullet7[] = {"ca\sounds\weapons\shells\big_shell_dirt_03", 0.0707946, 1, 15};
+		bullet8[] = {"ca\sounds\weapons\shells\big_shell_dirt_04", 0.0707946, 1, 15};
+		bullet9[] = {"ca\sounds\weapons\shells\big_shell_soft_01", 0.0707946, 1, 15};
+		bullet10[] = {"ca\sounds\weapons\shells\big_shell_soft_02", 0.0707946, 1, 15};
+		bullet11[] = {"ca\sounds\weapons\shells\big_shell_soft_03", 0.0707946, 1, 15};
+		bullet12[] = {"ca\sounds\weapons\shells\big_shell_soft_04", 0.0707946, 1, 15};
+		soundBullet[] = {"bullet1", 0.083, "bullet2", 0.083, "bullet3", 0.083, "bullet4", 0.083, "bullet5", 0.083, "bullet6", 0.083, "bullet7", 0.083, "bullet8", 0.083, "bullet9", 0.083, "bullet10", 0.083, "bullet11", 0.083, "bullet12", 0.083};
+		tMin = 1;
+		htMax = 600;
+		afMax = 0;
+		mfMax = 0;
+		mFact = 1;
+		tBody = 100;
+		scope = public;
+		model = "\ca\weapons_e\m60e4\m60e4";
+		picture = "\ca\weapons_E\Data\icons\m60e4_CA.paa";
+		handAnim[] = {"OFP2_ManSkeleton", "\Ca\weapons_E\Data\Anim\M60E4.rtm"};
+		UiPicture = "\CA\weapons\Data\Ico\i_mg_CA.paa";
+		displayName = $STR_EP1_DN_M60A4_EP1;
+		cursor = "MGCursor";
+		cursoraim = "\ca\Weapons\Data\clear_empty";
+		weaponInfoType = "RscWeaponZeroing";
+		discreteDistance[] = {100, 200, 300, 400, 500, 600, 700, 800};
+		discreteDistanceInitIndex = 2;
+		modes[] = {"manual", "close", "short", "medium", "far"};
+		
+		class manual : Mode_FullAuto {
+			recoil = "recoil_auto_machinegun_8outof10";
+			recoilProne = "recoil_auto_machinegun_prone_5outof10";
+			dispersion = 0.0008;
+			begin1[] = {"Ca\Sounds_E\Weapons_E\M60e4\m60e4_single1", 3.16228, 1, 1500};
+			soundBegin[] = {"begin1", 1};
+			soundContinuous = 0;
+			soundBurst = 0;
+			minRange = 0;
+			minRangeProbab = 0.3;
+			midRange = 5;
+			midRangeProbab = 0.58;
+			maxRange = 10;
+			maxRangeProbab = 0.04;
+			showToPlayer = true;
+			reloadTime = 0.086;
+			displayName = "";
+		};
+		
+		class close : manual {
+			burst = 10;
+			aiRateOfFire = 0.5;	// delay between shots at given distance
+			aiRateOfFireDistance = 50;
+			minRange = 10;
+			minRangeProbab = 0.05;
+			midRange = 20;
+			midRangeProbab = 0.58;
+			maxRange = 50;
+			maxRangeProbab = 0.04;
+			showToPlayer = false;
+		};
+		
+		class short : close {
+			burst = 8;
+			aiRateOfFire = 2;	// delay between shots at given distance
+			aiRateOfFireDistance = 300;
+			minRange = 50;
+			minRangeProbab = 0.05;
+			midRange = 150;
+			midRangeProbab = 0.58;
+			maxRange = 300;
+			maxRangeProbab = 0.04;
+		};
+		
+		class medium : close {
+			burst = 12;
+			aiRateOfFire = 3;	// delay between shots at given distance
+			aiRateOfFireDistance = 600;
+			minRange = 200;
+			minRangeProbab = 0.05;
+			midRange = 400;
+			midRangeProbab = 0.58;
+			maxRange = 600;
+			maxRangeProbab = 0.04;
+		};
+		
+		class far : close {
+			burst = 16;
+			aiRateOfFire = 5;	// delay between shots at given distance
+			aiRateOfFireDistance = 1000;
+			minRange = 400;
+			minRangeProbab = 0.05;
+			midRange = 600;
+			midRangeProbab = 0.4;
+			maxRange = 900;
+			maxRangeProbab = 0.01;
+		};
+		aiDispersionCoefY = 21.0;
+		aiDispersionCoefX = 21.0;
+		dexterity = 0.51;
+		type = 1	 + 	4;
+		reloadMagazineSound[] = {"Ca\Sounds_E\Weapons_E\M60e4\M60e4_reload", 0.0562341, 1, 25};
+		magazines[] = {"100Rnd_762x51_M240"};
+		
+		class Library {
+			libTextDesc = $STR_EP1_LIB_M60A4;
+		};
+		descriptionShort = $STR_EP1_DSS_M60A4_EP1;
+	};
 
 	class MeleeHatchet;
 	class MeleeCrowbar: MeleeHatchet
@@ -495,12 +778,7 @@ class CfgWeapons {
 		};
 		descriptionShort="A tool consisting of a metal bar with a single curved end and flattened points, often with a small fissure on one or both ends for removing nails.";
 	};
-
-	class M60A4_EP1;
-	class M60A4_EP1_DZ : M60A4_EP1 {
-		type = "1";
-	};
-
+	
 	class Default;
 class Pistol;
 class M16A2 ;
@@ -1290,47 +1568,47 @@ discreteDistanceInitIndex = 2;
 };
 
 class vil_SR25 : vil_DRM_BASE {
-scope = public;
-model = "\vilas_wwp\vil_sr25";
-displayName = $STR_VIL_SR25;
-picture = "\vilas_wwp\ico\w_vil_sr25_ca.paa";
-visionMode[] = {"Normal"};
-modelOptics = "\ca\weapons\2Dscope_MilDot_9";
-class OpticsModes {
-	class Leu9X {
-		opticsID = 1;
-		useModelOptics = 1;
-		opticsPPEffects[] = {"OpticsCHAbera1", "OpticsBlur1"};
-		opticsZoomInit = 0.083;
-		opticsZoomMin = 0.0277;
-		opticsZoomMax = 0.083;
-		distanceZoomMin = 400;
-		distanceZoomMax = 120;
-		memoryPointCamera = "Konec hlavne";
-		opticsFlare = true;
-		opticsDisablePeripherialVision = true;
-		cameraDir = "";
-		visionMode[] = {"Normal"};
-		discreteDistance[] = {100, 200, 300, 400, 500, 600, 700, 800};
-		discreteDistanceInitIndex = 2;
+	scope = public;
+	model = "\vilas_wwp\vil_sr25";
+	displayName = $STR_VIL_SR25;
+	picture = "\vilas_wwp\ico\w_vil_sr25_ca.paa";
+	visionMode[] = {"Normal"};
+	modelOptics = "\ca\weapons\2Dscope_MilDot_9";
+	class OpticsModes {
+		class Leu9X {
+			opticsID = 1;
+			useModelOptics = 1;
+			opticsPPEffects[] = {"OpticsCHAbera1", "OpticsBlur1"};
+			opticsZoomInit = 0.083;
+			opticsZoomMin = 0.0277;
+			opticsZoomMax = 0.083;
+			distanceZoomMin = 400;
+			distanceZoomMax = 120;
+			memoryPointCamera = "Konec hlavne";
+			opticsFlare = true;
+			opticsDisablePeripherialVision = true;
+			cameraDir = "";
+			visionMode[] = {"Normal"};
+			discreteDistance[] = {100, 200, 300, 400, 500, 600, 700, 800};
+			discreteDistanceInitIndex = 2;
+		};
 	};
-};
 
-class Single: Single  {
-	begin1[] = {"\vilas_wwp\sounds\sr25_fire1", 1.77828, 1, 1000};
-	soundBegin[] = {"begin1", 1.0};
-	midRange = 300;
-	maxRange = 500;
-	aiRateOfFireDistance = 500;
-};
+	class Single: Single  {
+		begin1[] = {"\vilas_wwp\sounds\sr25_fire1", 1.77828, 1, 1000};
+		soundBegin[] = {"begin1", 1.0};
+		midRange = 300;
+		maxRange = 500;
+		aiRateOfFireDistance = 500;
+	};
 
-class Library {
-	libTextDesc = $STR_VIL_SR25_LIB;
-};
-class Armory {
-	author = $STR_AUTHOR_VILAS;
-};
-descriptionShort = $STR_VIL_SR25_SHORT;
+	class Library {
+		libTextDesc = $STR_VIL_SR25_LIB;
+	};
+	class Armory {
+		author = $STR_AUTHOR_VILAS;
+	};
+	descriptionShort = $STR_VIL_SR25_SHORT;
 };
 
 class vil_M110 : vil_SR25 {
@@ -2347,7 +2625,7 @@ class Armory {
 };
 
 class vil_D_C7 : M16_base {
-picture = "\ca\Weapons\Data\equip\w_m4_ca.paa";
+picture = "\z\addons\dayz_code\textures\C7_ico.paa";
 UiPicture = "\CA\weapons\data\Ico\i_regular_CA.paa";
 
 displayName = "C7";
@@ -2371,7 +2649,7 @@ mfmax = 0;
 tbody = 100;
 };
 class vil_D_C8 : M16_base {
-picture = "\ca\Weapons\Data\equip\w_m4_ca.paa";
+picture = "\z\addons\dayz_code\textures\C8_ico.paa";
 UiPicture = "\CA\weapons\data\Ico\i_regular_CA.paa";
 
 displayName = "C8";
@@ -2438,25 +2716,26 @@ tbody = 100;
 
 class M16A2GL;
 class vil_D_C7_AG : M16A2GL {
-displayName = "C7 AG";
-scope = 2;		
-modes[] = {"Single", "FullAuto"};
-model = "\dutch_pack\vil_c7_ag";
-weaponInfoType = "RscWeaponEmpty";
-discreteDistance[] = {300};
-discreteDistanceInitIndex = 0;
-opticsZoomMax = 0.0732;
-distanceZoomMax = 300;
-opticsZoomMin = 0.0732;
-distanceZoomMin = 300;
-opticsZoomInit = 0.0732;
-modelOptics = "\Ca\weapons_E\M249\M145.p3d";
-afmax = 0;
-htmax = 480;
-htmin = 1;
-mfact = 1;
-mfmax = 0;
-tbody = 100;
+	picture = "\z\addons\dayz_code\textures\C7AG_ico.paa";
+	displayName = "C7 AG";
+	scope = 2;		
+	modes[] = {"Single", "FullAuto"};
+	model = "\dutch_pack\vil_c7_ag";
+	weaponInfoType = "RscWeaponEmpty";
+	discreteDistance[] = {300};
+	discreteDistanceInitIndex = 0;
+	opticsZoomMax = 0.0732;
+	distanceZoomMax = 300;
+	opticsZoomMin = 0.0732;
+	distanceZoomMin = 300;
+	opticsZoomInit = 0.0732;
+	modelOptics = "\Ca\weapons_E\M249\M145.p3d";
+	afmax = 0;
+	htmax = 480;
+	htmin = 1;
+	mfact = 1;
+	mfmax = 0;
+	tbody = 100;
 };
 
 
@@ -2492,23 +2771,6 @@ tbody = 100;
 		class Full;
 	};
 
-	class PK : Rifle {
-		class manual;
-		class close;
-		class short;
-		class medium;
-		class far;
-	};
-
-	class PK_DZ : PK {
-		type = "1";
-	};
-	class Pecheneg;
-	class Pecheneg_DZ : Pecheneg {
-		type = 1;
-	};
-
-	
 	class vil_AKM_BASE : AK_47_M {
 		scope = private;
 		weaponInfoType = "RscWeaponZeroing";
